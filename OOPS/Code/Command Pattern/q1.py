08aadcd5afdff0a10e583c8846e1d52513b5ed77
jinshah0322 <jinshah0322@gmail.com>
2024-01-15 11:50:22 +0530

    completed command pattern

---
+++ OOPS/Code/Command Pattern/q1.py
@@ -0,0 +1,46 @@
+from abc import ABC,abstractmethod
+
+class Command:
+    @abstractmethod
+    def execute(self):
+        pass
+class LightOnCommand(Command):
+    def __init__(self, light):
+        self.light = light
+
+    def execute(self):
+        self.light.turn_on()
+
+class LightOffCommand(Command):
+    def __init__(self, light):
+        self.light = light
+
+    def execute(self):
+        self.light.turn_off()
+class Light:
+    def turn_on(self):
+        print("Light is ON")
+
+    def turn_off(self):
+        print("Light is OFF")
+class RemoteControl:
+    def __init__(self):
+        self.command = None
+
+    def set_command(self, command):
+        self.command = command
+
+    def press_button(self):
+        self.command.execute()
+
+light = Light()
+light_on = LightOnCommand(light)
+light_off = LightOffCommand(light)
+
+remote = RemoteControl()
+
+remote.set_command(light_on)
+remote.press_button()  
+
+remote.set_command(light_off)
+remote.press_button()  
