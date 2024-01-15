9915bf186199bf5751607601226c6a2c70dcba9a
jinshah0322 <jinshah0322@gmail.com>
2024-01-15 18:27:51 +0530

    completed adapter pattern

---
+++ OOPS/Code/Adapter Method/q2.py
@@ -0,0 +1,24 @@
+import zope.interface
+
+class Target(zope.interface.Interface):
+    def request(self):
+        pass
+
+class Adaptee:
+    def specific_request(self):
+        return "Adaptee's specific request"
+    
+@zope.interface.implementer(Target)
+class Adapter:
+    def __init__(self,adaptee) -> None:
+        self.adaptee = adaptee
+
+    def request(self):
+        return f"Adaptee: {self.adaptee.specific_request()}"
+    
+def clientCode(target):
+    print(target.request())
+
+adaptee = Adaptee()
+adapter = Adapter(adaptee)
+clientCode(adapter)
