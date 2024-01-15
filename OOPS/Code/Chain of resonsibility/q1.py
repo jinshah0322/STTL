28330e500b23b6650c5ad9ee4ddf38efa229a6e3
jinshah0322 <jinshah0322@gmail.com>
2024-01-15 15:45:30 +0530

    completed chain of responsibility

---
+++ OOPS/Code/Chain of resonsibility/q1.py
@@ -0,0 +1,35 @@
+from abc import ABC,abstractmethod
+
+class Handler:
+    @abstractmethod
+    def setSuccessor(self,successor):
+        pass
+    
+    @abstractmethod
+    def handleRequest(self,request):
+        pass
+
+class concreteHandler1(Handler):
+    def setSuccessor(self, successor):
+        self.successor = successor
+
+    def handleRequest(self, request):
+        if(request=="handler1"):
+            print("ConcreteHandler1 is handling the request")
+        elif self.successor is not None:
+            self.successor.handleRequest(request)
+
+class concreteHandler2(Handler):
+    def setSuccessor(self, successor):
+        self.successor = successor
+    
+    def handleRequest(self, request):
+        if(request=="handler2"):
+            print("ConcreteHandler2 is handling the request")
+        elif self.successor is not None:
+            self.successor.handleRequest(request)
+
+handler1 = concreteHandler1()
+handler2 = concreteHandler2()
+handler1.setSuccessor(handler2)
+handler1.handleRequest("handler1")
