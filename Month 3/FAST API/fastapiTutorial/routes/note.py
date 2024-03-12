802bd8545bbdf4441dc6c2a478915d66eb2862dc
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2024-03-12 10:41:14 +0530

    completed add and read using fast api

--- Month 3/FAST API/fastapiTutorial/routes/note.py
+++ Month 3/FAST API/fastapiTutorial/routes/note.py
@@ -1,21 +1,18 @@
-from fastapi import APIRouter, Request
-from models.note import Note
+from fastapi import APIRouter, Request, Form
 from config.db import conn
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
 
 note = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
 @note.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
+async def read_note(request: Request):
     notes = conn.notes.notes.find({})
     notesList = []
     for note in notes:
         notesList.append({
-            "_id": note["_id"],
             "title": note["title"],
             "desc": note["desc"]
         })
@@ -23,3 +20,12 @@ async def read_item(request: Request):
         "index.html",
         {"request": request, "notes": notesList}
     )
+
+
+@note.post("/")
+async def add_note(request: Request):
+    form = await request.form()
+    form_dict = dict(form)
+    form_dict["important"] = True if form_dict.get("important") == "on" else False
+    conn.notes.notes.insert_one(form_dict)
+    return {"msg": "Note added successfully", "success": True, "status": 200}
